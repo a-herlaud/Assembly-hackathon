d1bc9346b77a99aa354c75ff8340191a4e873388
aherlaud <aherlaud@student.42.fr>
2026-07-02 00:22:33 +0200

    feat(mcp): test mcp connexion
    - srcs/agent/script/test.py: mcp test and show all tools in it
    - docker-compose.yml: add a volume on an external drive

--- srcs/agent/script/test.py
+++ srcs/agent/script/test.py
@@ -1,1 +1,23 @@
-print("hello")
+import asyncio
+from mcp import ClientSession
+from mcp.client.streamable_http import streamablehttp_client
+
+
+async def main():
+    async with streamablehttp_client(
+        "https://mcp.code4code.eu/mcp"
+    ) as (read, write, _):
+
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+
+            tools = await session.list_tools()
+
+            for tool in tools.tools:
+                print(tool.name)
+                print(tool.description)
+                print(tool.inputSchema)
+                print("-" * 40)
+
+
+asyncio.run(main())
